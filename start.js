85b3280b1a7da110afd60e00ec349abefd832f71
isacfarias <isac.farias.21@gmail.com>
2021-01-16 16:11:55 -0400

    exemplo inicial criando uma funçao em ts e convertendo para js

---
+++ start.js
@@ -0,0 +1,4 @@
+function falaQueroAgua(bebida) {
+    console.log("Desce uma " + bebida + " gelada ae meu rei");
+}
+falaQueroAgua('agua');
---
+++ start.ts
@@ -0,0 +1,5 @@
+function falaQueroAgua(bebida: string) {
+    console.log(`Desce uma ${bebida} gelada ae meu rei`);
+}
+
+falaQueroAgua('agua');
