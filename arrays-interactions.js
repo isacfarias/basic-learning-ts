75f3cdffcf884dce2932d2ae5f6d0b2212aad47d
isacfarias <isac.farias.21@gmail.com>
2021-01-16 20:23:48 -0400

    conceito basico sobre interaçao por indice e valor

---
+++ arrays-interactions.js
@@ -0,0 +1,10 @@
+var listaMercado = ["arroz", "feijão", "trigo", "farinha", "batata"];
+//indices
+for (var i in listaMercado) {
+    console.log(i);
+}
+//valores
+for (var _i = 0, listaMercado_1 = listaMercado; _i < listaMercado_1.length; _i++) {
+    var produto = listaMercado_1[_i];
+    console.log(produto);
+}
---
+++ arrays-interactions.ts
@@ -0,0 +1,11 @@
+let listaMercado = ["arroz", "feijão", "trigo", "farinha", "batata"];
+
+//indices
+for (let i in listaMercado) {
+    console.log(i);
+}
+
+//valores
+for (let produto of listaMercado) {
+    console.log(produto);
+}
